86f4726cf99d85ed070716d303a158ef53fe9c5d
dmitry-shag <dmit.shag@gmail.com>
2015-06-07 19:57:55 +0300

    create_program

---
+++ TenderIt/Models/Category.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TenderIt.Models
+{
+    public class Category
+    {
+        public string Name { get; set; }
+    }
+
+    public class Company
+    {
+        public string ShortName { get; set; }
+        public string FullName { get; set; }
+        public string Communications { get; set; }
+        public Guid UserId { get; set; }
+        public string ImageLink { get; set; }
+        public string Description { get; set; }
+        public int? Likes { get; set; }
+        public int? Unlikes { get; set; }
+    }
+
+    public class CompanyFeedBack
+    {
+        public Guid CompanyId { get; set; }
+        public Guid UserId { get; set; }
+        public string CompanyId { get; set; }
+
+    }
+
+    public class CompanyInCategories
+    {
+        public Guid CompanyId { get; set; }
+        public Guid UserId { get; set; }
+    }
+
+    public class ProductTable
+    {
+        public string Name { get; set; }
+        public Guid? CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Tender
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public decimal MaxPrice { get; set; }
+        public int Quantity { get; set; }
+        public string ImageLink { get; set; }
+        public Guid CategoryId { get; set; }
+        public DateTime StatDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Check { get; set; }
+
+    }
+
+    public class TenderOffer
+    {
+        public Guid TanderId { get; set; }
+        public Guid CompanyId { get; set; }
+        public string Offer { get; set; }
+    }
+
+    public class UserProfile
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Communications { get; set; }
+        public Guid UserId { get; set; }
+
+    }
+
+    public class Users
+    {
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public int isBlocked { get; set; }
+        public int PasswordCount { get; set; }
+
+    }
+
+
+}
---
+++ TenderIt/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("TenderIt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MICROSOFT")]
+[assembly: AssemblyProduct("TenderIt")]
+[assembly: AssemblyCopyright("© MICROSOFT, 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID-идентификации библиотеки типов, если этот проект видим для COM
+[assembly: Guid("94bdfe11-e4a1-4d1c-b95c-71fdf63fdf95")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
